68df83c03d448e2627727690ac0f85bb1ac81e0b
iharayuki10044 <iharayuki0103@gmail.com>
2022-10-07 14:46:01 +0900

    first commit

---
+++ include/odom_publisher_unity/odom_publisher_unity.h
@@ -0,0 +1,17 @@
+#ifndef __ODOM_PUBLISHER_UNITY_H__
+#define __ODOM_PUBLISHER_UNITY_H__
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+
+class OdomPublisherUnity{
+    public:
+        OdomPublisherUnity();
+    private:
+
+};
+
+#endif // __ODOM_PUBLISHER_UNITY_H__
---
+++ src/odom_publisher_unity.cpp
@@ -0,0 +1,10 @@
+include "odom_publisher_unity/odom_publisher_unity.h"
+
+
+int main(int argc, char** argv){
+    // ros::init(argc, argv, "odom_publisher_unity");
+    // ros::NodeHandle nh;
+    // OdomPublisherUnity odom_publisher_unity;
+    // ros::spin();
+    return 0;
+}
