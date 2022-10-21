fb6bbe3232a6a884eef239982284b106a28c57ff
iharayuki10044 <iharayuki0103@gmail.com>
2022-10-21 17:39:32 +0900

    complete

--- include/odom_publisher_unity/odom_publisher_unity.h
+++ include/odom_publisher_unity/odom_publisher_unity.h
@@ -2,6 +2,11 @@
 #define __ODOM_PUBLISHER_UNITY_H__
 
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+
+
+
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
@@ -17,27 +22,35 @@ class OdomPublisherUnity{
 
         void unity_callback(const geometry_msgs::PoseStamped&);
 
+        void odom_callback(const nav_msgs::Odometry&);
+
     private:
         double HZ;
         double tf_roatation;
 
         bool unity_subscribed;
+        bool odom_subscribed; 
         bool is_firstmsgs;
 
         geometry_msgs::PoseStamped unity_pose;
         geometry_msgs::PoseStamped init_unity_pose;
         geometry_msgs::PoseStamped unity_odom_pose;
 
+        nav_msgs::Odometry odom_pose;
+
         ros::NodeHandle private_nh;
         
         ros::Subscriber unity_sub;
         ros::Publisher unity_odom_pub;
 
+        ros::Subscriber odom_sub;
+        ros::Publisher pose_pub;
+
         //TF Broadcasterの実体化
         // tf unity to map 
         tf2_ros::TransformBroadcaster robot_pose_broadcaster;
 
-        // tf map to unity
+        // tf map to basefoot
         tf2_ros::TransformBroadcaster map_to_basefoot_broadcaster;
 
 
--- src/odom_publisher_unity.cpp
+++ src/odom_publisher_unity.cpp
@@ -5,110 +5,57 @@ OdomPublisherUnity::OdomPublisherUnity()
 {
     private_nh.param("HZ", HZ, 10.0);
 
-    // odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 1);
-    unity_sub = private_nh.subscribe("/pose_unit04", 1, &OdomPublisherUnity::unity_callback, this);
+    odom_sub = private_nh.subscribe("/odom_unit04", 1, &OdomPublisherUnity::odom_callback, this);     
     // odom_tf_pub = new tf::TransformBroadcaster();
     // odom_tf_listener = new tf::TransformListener();
 
-    unity_odom_pub = private_nh.advertise<geometry_msgs::PoseStamped>("/pose", 1);
+    pose_pub = private_nh.advertise<geometry_msgs::PoseStamped>("/pose", 1);
 
     unity_subscribed = false;
     is_firstmsgs = true;
 
 }
 
-void OdomPublisherUnity::unity_callback(const geometry_msgs::PoseStamped& msg)
+
+void OdomPublisherUnity::odom_callback(const nav_msgs::Odometry& msg)
 {
-    unity_pose = msg;
-    unity_subscribed = true;
+    odom_pose = msg;
+    odom_subscribed = true;
 
-    if(is_firstmsgs){
-        init_unity_pose = msg;
-        init_unity_pose.header.frame_id = "Unity";
-    
-        //  //Robot位置と姿勢(x,y,yaw)の取得
-        // double x = unity_pose.pose.position.x;
-        // double y = unity_pose.pose.position.y;
-        // double z = unity_pose.pose.position.z;
- 
-        // //yawのデータからクォータニオンを作成
-        // geometry_msgs::Quaternion robot_quat=unity_pose.pose.orientation;
-        // geometry_msgs::Quaternion transform_quat = robot_quat;
+    geometry_msgs::PoseStamped pose_stamped;    
 
-        // //map座標系の元となるロボットの位置姿勢情報格納用変数の作成
-        // geometry_msgs::TransformStamped map_origin_pose_tf;
+    pose_stamped.header = odom_pose.header;
+    pose_stamped.pose = odom_pose.pose.pose;
 
-        // //現在の時間の格納
-        // map_origin_pose_tf.header.stamp = ros::Time::now();
+    // robot位置と姿勢(クォータニオン)の取得
+    double x = pose_stamped.pose.position.x;
+    double y = pose_stamped.pose.position.y;
+    double z = pose_stamped.pose.position.z;
+    geometry_msgs::Quaternion q = pose_stamped.pose.orientation;
 
-        // //座標系unityとmapの指定
-        // map_origin_pose_tf.header.frame_id = "Unity";
-        // map_origin_pose_tf.child_frame_id  = "map";
+    // robot座標系の原点となるロボットの位置姿勢情報格納用変数
+    geometry_msgs::TransformStamped robot_pose;
 
-        // //Unity座標系からみたmap座標系の原点位置と方向の格納
-        // map_origin_pose_tf.transform.translation.x = x;
-        // map_origin_pose_tf.transform.translation.y = y;
-        // map_origin_pose_tf.transform.translation.z = z;
-        // map_origin_pose_tf.transform.rotation = robot_quat;
+    // 現在時間の格納 
+    robot_pose.header.stamp = ros::Time::now();
 
-        // //tf情報をbroadcast(座標系の設定)
-        // robot_pose_broadcaster.sendTransform(map_origin_pose_tf);
+    // 座標系map と　robot の指定
+    robot_pose.header.frame_id = "map";
+    robot_pose.child_frame_id = "base_footprint";
 
-        // double roll, pitch, yaw;
-        // tf::Matrix3x3(tf::Quaternion(robot_quat.x, robot_quat.y, robot_quat.z, robot_quat.w)).getRPY(roll, pitch, yaw);
+    robot_pose.transform.translation.x = x;
+    robot_pose.transform.translation.y = y;
+    robot_pose.transform.translation.z = z;
+    robot_pose.transform.rotation = q;
 
-        // std::cout << "init_unity_roll : "<< roll / 3.1415 *180 << std::endl;
-        // std::cout << "init_unity_pitch : "<< pitch / 3.1415 *180 << std::endl;
-        // std::cout << "init_unity_yaw : " << yaw /  3.1415 *180 << std::endl;
+    // ロボットの位置姿勢情報をTFに送信
+    robot_pose_broadcaster.sendTransform(robot_pose);
 
-        // is_firstmsgs = false;
-    
-    }
+    pose_pub.publish(pose_stamped);
 
+    // std::cout << "odom tf send ok" << std::endl;
 
-    unity_odom_pose = msg;
-    unity_odom_pose.pose.position.x = unity_pose.pose.position.x - init_unity_pose.pose.position.x;
-    unity_odom_pose.pose.position.y = unity_pose.pose.position.y - init_unity_pose.pose.position.y;
-    unity_odom_pose.pose.position.z = unity_pose.pose.position.z - init_unity_pose.pose.position.z;
-
-    unity_odom_pose.header.stamp = ros::Time::now();
-    unity_odom_pose.header.frame_id = "map";
-
-    unity_odom_pub.publish(unity_odom_pose);
-    
-    //Robot位置と姿勢(x,y,yaw)の取得
-    double x = unity_odom_pose.pose.position.x;
-    double y = unity_odom_pose.pose.position.y;
-    double z = unity_odom_pose.pose.position.z;
- 
-    //yawのデータからクォータニオンを作成
-    geometry_msgs::Quaternion now_quat;
-    now_quat.w = 0.0;
-    now_quat.x = 0.0;
-    now_quat.y = 0.0;
-    now_quat.z = 1.0;
-    
-    //robot座標系の元となるロボットの位置姿勢情報格納用変数の作成
-    geometry_msgs::TransformStamped robotState;
-    
-    //現在の時間の格納
-    robotState.header.stamp = ros::Time::now();
-    
-    //座標系globalとrobotの指定
-    robotState.header.frame_id = "map";
-    robotState.child_frame_id  = "base_footprint";
-    
-    //global座標系からみたrobot座標系の原点位置と方向の格納
-    robotState.transform.translation.x = x;
-    robotState.transform.translation.y = y;
-    robotState.transform.translation.z = z;
-    robotState.transform.rotation = unity_pose.pose.orientation;
-    // robotState.transform.rotation = now_quat;
-
-    //tf情報をbroadcast(座標系の設定)
-    robot_pose_broadcaster.sendTransform(robotState);
-
-}
+} 
 
 void OdomPublisherUnity::process()
 {
@@ -122,6 +69,10 @@ void OdomPublisherUnity::process()
             // std::cout<<"init_unity_pose_position: "<<init_unity_pose<<std::endl;
             // std::cout<<"unity_odom_pose.position: "<<unity_odom_pose<<std::endl; 
         }
+        if(odom_subscribed){
+            // std::cout<<"odom_pose.position: "<<odom_pose<<std::endl;
+        }
+
         ros::spinOnce();
         loop_rate.sleep();
     }
